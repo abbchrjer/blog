185deccffd293000d410d70fc129a2525401ae7c
abbchrjer <92145483+abbchrjer@users.noreply.github.com>
2022-04-22 12:37:43 +0200

    fix error

--- components/navbar.vue
+++ components/navbar.vue
@@ -28,7 +28,7 @@
 
     <div class=" flex flex-col justify-center lg:hidden" type="button" v-on:click="toggleNavbar()">
     <div class="relative sm:max-w-xl mx-auto">
-        <nav x-data="{ open: false }">
+        <nav>
             <button class="text-white w-10 h-10 relative focus:outline-none" @click="open = !open">
                 <div class="block w-5 absolute left-1/2 top-1/2   transform  -translate-x-1/2 -translate-y-1/2">
                     <span aria-hidden="true" class="block absolute h-0.5 w-5 bg-current transform transition duration-500 ease-in-out" :class="{'rotate-45': open,' -translate-y-1.5': !open }"></span>
@@ -106,6 +106,7 @@ export default {
   data() {
     return {
       showMenu: false,
+      open: false,
 
       options: ["Home", "Posts", "About"],
       hrefs: ["/home", "/blogposts", "/about"],
